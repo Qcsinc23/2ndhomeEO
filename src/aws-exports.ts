2fc9ea825f614fae91f0fc5182d63b8c158dafff
Qcsinc23 <sales@quietcraftsolutions.com>
2024-11-03 18:43:08 +0000

    appclientid changed

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,9 +3,9 @@ import { ResourcesConfig } from 'aws-amplify';
 const awsConfig: ResourcesConfig = {
     Auth: {
         Cognito: {
-            userPoolId: 'us-east-2_2qJVq1bVB',
-            userPoolClientId: 'd3n3i5jooxdsrv',
-            signUpVerificationMethod: 'code',
+            userPoolId: "us-east-2_2qJVq1bVB",
+            userPoolClientId: "6jlptfktguc8ocbt0mjbhdrolc",
+            signUpVerificationMethod: "code",
             loginWith: {
                 email: true,
                 phone: false,
