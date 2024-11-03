407a6425fd47af5f42155fa782bb9eaf28e9f918
Qcsinc23 <sales@quietcraftsolutions.com>
2024-11-03 19:04:47 +0000

    update userpool and userpool client id

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,8 +3,8 @@ import { ResourcesConfig } from 'aws-amplify';
 const awsConfig: ResourcesConfig = {
     Auth: {
         Cognito: {
-            userPoolId: "us-east-2_2qJVq1bVB",
-            userPoolClientId: "6jlptfktguc8ocbt0mjbhdrolc",
+            userPoolId: "us-east-2_F9nz52p0H",
+            userPoolClientId: "53hcnnsfvpr9rk64ofrpkjjji9",
             signUpVerificationMethod: "code",
             loginWith: {
                 email: true,
